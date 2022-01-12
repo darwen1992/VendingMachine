9b6a0575a841ba986b0b7ac847065fae6a04e044
darwinmwstafa <darwin.mustafa92@gmail.com>
2022-01-12 15:52:19 +0100

    This is updated last version and final version.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,10 +6,37 @@ public class App
     public static void main( String[] args )
     {
         Product1 p1=new Product1(1,2.3,"Cola");
-        Product2 p2=new Product2(2,5,"Pizza");
+        Product2 p2=new Product2(2,6,"Pizza");
         Product3 p3=new Product3(3,8,"Gum");
         Product p[]={p1,p2,p3};
         VendingMachineImpl ob= new VendingMachineImpl(p);
-        System.out.println( "Hello World!" );
+
+
+        String s[]=ob.getProduct();
+        System.out.println("List of Pruduct");
+        for (int i=0;i<s.length;i++){
+            System.out.println( s[i]);
+        }
+        System.out.println("_________________________________");
+
+        ob.addCurrency(10);
+        System.out.println("_________________________________");
+
+        System.out.println("Description is  "+ ob.getDescription(1));
+        System.out.println("_________________________________");
+
+        Product re=ob.request(2);
+        System.out.println( "Requested  "+ re.getId()+"  "+re.getProductName()+"  "+re.getPrice());
+        System.out.println("_________________________________");
+
+        System.out.println("Your balance is  "+ ob.getBalance());
+        System.out.println("_________________________________");
+
+        System.out.println("Endedd and your Change is  "+ ob.endSession());
+        System.out.println("_________________________________");
+
+
+
+        System.out.println( "Bye Bye");
     }
 }
--- src/main/java/org/example/VendingMachineImpl.java
+++ src/main/java/org/example/VendingMachineImpl.java
@@ -13,6 +13,7 @@ public class VendingMachineImpl implements VendingMachine {
     public void addCurrency(double amount) {
 if (amount==1||amount==2||amount==5||amount==10||amount==20||amount==50||amount==100||amount==200||amount==500||amount==1000){
      depositPool=depositPool+amount;
+    System.out.println("Deposit Added "+depositPool);
     }
 
     else{
@@ -27,8 +28,9 @@ if (amount==1||amount==2||amount==5||amount==10||amount==20||amount==50||amount=
         boolean fin=false;
         for (int i=0;i<products.length;i++){
             if (products[i].getId()==id){
-                if(products[i].getPrice()>=depositPool){
+                if(products[i].getPrice()<=depositPool){
                     fin=true;
+                    depositPool=depositPool-products[i].getPrice();
                     return products[i];
                 }
                 else {
@@ -56,7 +58,7 @@ if (amount==1||amount==2||amount==5||amount==10||amount==20||amount==50||amount=
 String disc="";
         for (int i=0;i<products.length;i++){
             if (products[i].getId()==id){
-                disc=products[i].getId()+products[i].getProductName()+products[i].getPrice();
+                disc=products[i].getId()+"  "+products[i].getProductName()+"  "+products[i].getPrice();
             }}
         return disc;
     }
@@ -70,9 +72,9 @@ String disc="";
     @Override
     public String[] getProduct() {
         String st[]=new String[3];
-        st[0]=products[0].getId()+products[0].getProductName()+products[0].getPrice();
-        st[1]=products[1].getId()+products[1].getProductName()+products[1].getPrice();
-        st[2]=products[2].getId()+products[2].getProductName()+products[2].getPrice();
+        st[0]=products[0].getId()+"  "+products[0].getProductName()+"  "+products[0].getPrice();
+        st[1]=products[1].getId()+"  "+products[1].getProductName()+"  "+products[1].getPrice();
+        st[2]=products[2].getId()+"  "+products[2].getProductName()+"  "+products[2].getPrice();
 
         return st;
     }
